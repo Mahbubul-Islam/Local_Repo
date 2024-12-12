2a5360686d8dea64f4ba43ce085b4ab1f58af106
Mahbubul Islam <mahbubulislamshiam48@gmail.com>
2024-12-13 01:57:44 +0600

    Delete the adding student option

--- student.cs
+++ student.cs
@@ -6,7 +6,7 @@ public class Class1
 	{
         Console.WriteLine("This is a student class");
 
-        Console.WriteLine("This is another line(add a student));"
+        
 
         Console.WriteLine("This is another line(remove a student));"
 
